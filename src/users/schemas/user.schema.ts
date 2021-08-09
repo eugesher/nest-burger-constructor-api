9e394a766aff3834b8ca71ced16d8575e0fdc71e
Evgenii Chernyshov <mailbox@eugesher.site>
2021-08-09 17:57:48 +0300

    feat: user schema

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,1 +1,21 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class User {
+  @Prop({
+    required: true,
+    unique: true,
+  })
+  email: string;
+
+  @Prop({
+    select: false,
+    required: true,
+  })
+  passwordHash: string;
+}
+
+export type UserDocument = User & Document;
+
+export const UserSchema = SchemaFactory.createForClass(User);
