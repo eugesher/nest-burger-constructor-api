f3b215fa79673819d2a08ad2d7d28983090e282a
Evgenii Chernyshov <mailbox@eugesher.site>
2021-08-09 18:56:25 +0300

    mongo connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,21 @@ import { UsersModule } from './users/users.module';
 import { IngredientsModule } from './ingredients/ingredients.module';
 import { BurgersModule } from './burgers/burgers.module';
 import { OrdersModule } from './orders/orders.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule, IngredientsModule, BurgersModule, OrdersModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/burgersdb', {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    }),
+    UsersModule,
+    IngredientsModule,
+    BurgersModule,
+    OrdersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
