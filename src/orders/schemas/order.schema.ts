0a383df0c22de61c10ceeda7c044dc15722d97b3
Evgenii Chernyshov <mailbox@eugesher.site>
2021-08-09 20:33:42 +0300

    feat: order schema

--- src/orders/schemas/order.schema.ts
+++ src/orders/schemas/order.schema.ts
@@ -1,1 +1,38 @@
-export class Order {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { Burger } from '../../burgers/schemas/burger.schema';
+import * as mongoose from 'mongoose';
+import { User } from '../../users/schemas/user.schema';
+import { Valuable } from '../../utils/Valuable';
+
+@Schema()
+export class Order extends Valuable {
+  @Prop({
+    type: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Burger',
+      },
+    ],
+    required: true,
+  })
+  list: Burger[];
+
+  @Prop({
+    required: true,
+    get: Valuable.getPrice,
+    set: Valuable.setPrice,
+  })
+  price: number;
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  })
+  owner: User;
+}
+
+export type OrderDocument = Order & Document;
+
+export const OrderSchema = SchemaFactory.createForClass(Order);
