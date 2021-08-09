448df0203637b0f808baf545d7b7e196c52c299e
Evgenii Chernyshov <mailbox@eugesher.site>
2021-08-09 20:19:47 +0300

    feat: burger schema

--- src/burgers/burgers.module.ts
+++ src/burgers/burgers.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BurgersService } from './burgers.service';
 import { BurgersController } from './burgers.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Burger, BurgerSchema } from './schemas/burger.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Burger.name, schema: BurgerSchema }]),
+  ],
   controllers: [BurgersController],
   providers: [BurgersService],
 })
--- src/burgers/schemas/burger.schema.ts
+++ src/burgers/schemas/burger.schema.ts
@@ -1,1 +1,44 @@
-export class Burger {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Valuable } from '../../utils/Valuable';
+import * as mongoose from 'mongoose';
+import { Ingredient } from '../../ingredients/schemas/ingredient.schema';
+import { User } from '../../users/schemas/user.schema';
+
+@Schema()
+export class Burger extends Valuable {
+  @Prop({
+    required: true,
+    minLength: 2,
+    maxLength: 100,
+  })
+  name: string;
+
+  @Prop({
+    type: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Ingredient',
+      },
+    ],
+    required: true,
+  })
+  ingredients: Ingredient[];
+
+  @Prop({
+    required: true,
+    get: Valuable.getPrice,
+    set: Valuable.setPrice,
+  })
+  price: number;
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  })
+  owner: User;
+}
+
+export type BurgerDocument = Burger & Document;
+
+export const BurgerSchema = SchemaFactory.createForClass(Burger);
